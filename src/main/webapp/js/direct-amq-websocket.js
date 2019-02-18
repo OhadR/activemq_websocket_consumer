795190106183c6cd95854e0e3b3ec5179806a6ec
OhadR-Cellebrite-PC <ohad.redlich@cellebrite.com>
2019-02-18 17:27:10 +0200

    add apache-activemq's 'stomp.js', and write my JS that uses it (the client)

---
+++ src/main/webapp/js/direct-amq-websocket.js
@@ -0,0 +1,84 @@
+var intervalObj;
+var ws;
+
+var counter = 0;
+var destination;
+var client;
+
+function connect() 
+{
+	client = Stomp.client( "ws://localhost:61614/stomp", "v11.stomp" );
+	client.connect( "", "",
+	 function() {
+	     client.subscribe("jms.topic.test",
+	      function( message ) 
+	      {
+//	    	 	alert( event );
+				var log = document.getElementById("log");
+				console.log('received message from ' + destination + '. ' + message.body);
+				log.innerHTML += message.body + "\n";
+			}, 
+	   { priority: 9 } 
+	     );
+	 }
+	);
+
+	
+/*    var host = document.location.host;
+    var pathname = document.location.pathname;
+    
+    ws = new WebSocket("ws://" +host  + pathname + "amq/" + destination);
+
+    ws.onmessage = function(event) {
+    	var log = document.getElementById("log");
+    	console.log('received message from ' + destination + '. ' + event.data);
+        var message = JSON.parse(event.data);
+        log.innerHTML += message + "\n";
+    };*/
+}
+
+$(document).ready(function() {
+
+	destination = $('#queue_name').val();
+
+	$("#submit").click(function(){
+		
+
+	});
+});
+
+function sendMessage()
+{
+	console.log('sending message to ' + destination);
+	++counter;
+	client.send("jms.topic.test", { priority: 9 }, "Pub/Sub over STOMP! " + counter);
+}
+
+function getServerAddress()
+{
+	var serverAddress;
+	var isSecured = $('#server_secured').is(":checked");		//is https (o/w http)
+	if(isSecured)
+		serverAddress = "https://";
+	else
+		serverAddress = "http://";
+	
+	var serverName = $('#server_name').val();
+	var serverPort = $('#server_port').val();
+	serverAddress += serverName;
+//	if(!serverPort)		//checks null, undefuned and empty string (tnx @Slava!)
+	serverAddress += ':' + serverPort;		//if 'port' is empty it does not harm (http://host:/rest.. is OK
+	return serverAddress;
+}
+
+
+function registerListener()
+{
+	connect();
+}
+
+function unregisterListener()
+{
+	disconnect();
+}
+
