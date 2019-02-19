6186746dacf453dc75ac46c00af36a5decddca30
CELLEBRITE-LT\OhadR <ohad.redlich@cellebrite.com>
2019-02-19 23:30:45 +0200

    remove junk from JS + handle disconnection

--- src/main/webapp/js/direct-amq-websocket.js
+++ src/main/webapp/js/direct-amq-websocket.js
@@ -1,6 +1,7 @@
 var counter = 0;
 var destination;
 var client;
+var log;
 
 function connect() 
 {
@@ -11,7 +12,6 @@ function connect()
 	      function( message ) 
 	      {
 //	    	 	alert( event );
-				var log = document.getElementById("log");
 				console.log('received message from ' + destination + '. ' + message.body);
 				log.innerHTML += message.body + "\n";
 			}, 
@@ -19,25 +19,13 @@ function connect()
 	     );
 	 }
 	);
-
-	
-/*    var host = document.location.host;
-    var pathname = document.location.pathname;
-    
-    ws = new WebSocket("ws://" +host  + pathname + "amq/" + destination);
-
-    ws.onmessage = function(event) {
-    	var log = document.getElementById("log");
-    	console.log('received message from ' + destination + '. ' + event.data);
-        var message = JSON.parse(event.data);
-        log.innerHTML += message + "\n";
-    };*/
 }
 
 $(document).ready(function() {
 
 	$('#queue_name').val('ohadr-test-websocket');
 	destination = $('#queue_name').val();
+	log = document.getElementById("log");
 
 	$("#submit").click(function(){
 		
@@ -52,31 +40,19 @@ function sendMessage()
 	client.send(destination, { priority: 9 }, "Pub/Sub over STOMP! " + counter);
 }
 
-function getServerAddress()
-{
-	var serverAddress;
-	var isSecured = $('#server_secured').is(":checked");		//is https (o/w http)
-	if(isSecured)
-		serverAddress = "https://";
-	else
-		serverAddress = "http://";
-	
-	var serverName = $('#server_name').val();
-	var serverPort = $('#server_port').val();
-	serverAddress += serverName;
-//	if(!serverPort)		//checks null, undefuned and empty string (tnx @Slava!)
-	serverAddress += ':' + serverPort;		//if 'port' is empty it does not harm (http://host:/rest.. is OK
-	return serverAddress;
-}
-
-
 function registerListener()
 {
 	connect();
 }
 
-function unregisterListener()
+function disconnect()
 {
-	disconnect();
+	console.log('disconnecting...');
+	client.disconnect( disconnectCallback );
 }
 
+function disconnectCallback()
+{
+	console.log('disconnected.');
+	log.innerHTML += 'disconnected.\n';
+}
